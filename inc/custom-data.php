36207b0f07a0ae4db01f96a4a127c5567ce362f6
woodwardtw <twwoodward@me.com>
2022-01-29 13:28:37 -0500

    responsive carousel multi element

--- inc/custom-data.php
+++ inc/custom-data.php
@@ -14,7 +14,7 @@ function create_project_cpt() {
 $labels = array(
     'name' => __( 'project', 'Post Type General Name', 'textdomain' ),
     'singular_name' => __( 'project', 'Post Type Singular Name', 'textdomain' ),
-    'menu_name' => __( 'Project', 'textdomain' ),
+    'menu_name' => __( 'Projects', 'textdomain' ),
     'name_admin_bar' => __( 'project', 'textdomain' ),
     'archives' => __( 'project Archives', 'textdomain' ),
     'attributes' => __( 'project Attributes', 'textdomain' ),
@@ -69,3 +69,65 @@ $labels = array(
   $wp_rewrite->flush_rules();
 }
 add_action( 'init', 'create_project_cpt', 0 );
+
+// Register Custom Post Type people
+// Post Type Key: methods
+function create_methods_cpt() {
+$labels = array(
+    'name' => __( 'Methods', 'Post Type General Name', 'textdomain' ),
+    'singular_name' => __( 'Methods', 'Post Type Singular Name', 'textdomain' ),
+    'menu_name' => __( 'Methods', 'textdomain' ),
+    'name_admin_bar' => __( 'methods', 'textdomain' ),
+    'archives' => __( 'Methods Archives', 'textdomain' ),
+    'attributes' => __( 'Methods Attributes', 'textdomain' ),
+    'parent_item_colon' => __( 'methods:', 'textdomain' ),
+    'all_items' => __( 'All Methods', 'textdomain' ),
+    'add_new_item' => __( 'Add New Method', 'textdomain' ),
+    'add_new' => __( 'Add New', 'textdomain' ),
+    'new_item' => __( 'New Method', 'textdomain' ),
+    'edit_item' => __( 'Edit Method', 'textdomain' ),
+    'update_item' => __( 'Update Method', 'textdomain' ),
+    'view_item' => __( 'View Method', 'textdomain' ),
+    'view_items' => __( 'View Methods', 'textdomain' ),
+    'search_items' => __( 'Search Methods', 'textdomain' ),
+    'not_found' => __( 'Not found', 'textdomain' ),
+    'not_found_in_trash' => __( 'Not found in Trash', 'textdomain' ),
+    'featured_image' => __( 'Featured Image', 'textdomain' ),
+    'set_featured_image' => __( 'Set featured image', 'textdomain' ),
+    'remove_featured_image' => __( 'Remove featured image', 'textdomain' ),
+    'use_featured_image' => __( 'Use as featured image', 'textdomain' ),
+    'insert_into_item' => __( 'Insert into methods', 'textdomain' ),
+    'uploaded_to_this_item' => __( 'Uploaded to this methods', 'textdomain' ),
+    'items_list' => __( 'People list', 'textdomain' ),
+    'items_list_navigation' => __( 'methods list navigation', 'textdomain' ),
+    'filter_items_list' => __( 'Filter methods list', 'textdomain' ),
+  );
+  $args = array(
+    'label' => __( 'Methods', 'textdomain' ),
+    'description' => __( '', 'textdomain' ),
+    'labels' => $labels,
+    'menu_icon' => '',
+    'supports' => array('title', 'editor', 'revisions', 'author', 'trackbacks', 'custom-fields', 'thumbnail',),
+    'taxonomies' => array('category', 'post_tag'),
+    'public' => true,
+    'show_ui' => true,
+    'show_in_menu' => true,
+    'menu_position' => 5,
+    'show_in_admin_bar' => true,
+    'show_in_nav_menus' => true,
+    'can_export' => true,
+    'has_archive' => true,
+    'hierarchical' => false,
+    'exclude_from_search' => false,
+    'show_in_rest' => true,
+    'publicly_queryable' => true,
+    'capability_type' => 'post',
+    'menu_icon' => 'dashicons-admin-users',
+  );
+  register_post_type( 'methods', $args );
+  
+  // flush rewrite rules because we changed the permalink structure
+  global $wp_rewrite;
+  $wp_rewrite->flush_rules();
+}
+add_action( 'init', 'create_methods_cpt', 0 );
--- loop-templates/content-project.php
+++ loop-templates/content-project.php
@@ -28,34 +28,78 @@ defined( 'ABSPATH' ) || exit;
             ?>
         </div>
         <div class="row purple-row methods">
-            <h1 id="methods">Methods</h1>
-            <!--FULL THING TO DO IT RIGHT AT https://www.codeply.com/p/0CWffz76Q9 -->
-        <div id="methodsIndicators" class="carousel slide" data-bs-ride="carousel">
-            <div class="carousel-indicators">
-                <button type="button" data-bs-target="#methodsIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-                <button type="button" data-bs-target="#methodsIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-                <button type="button" data-bs-target="#methodsIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
-            </div>
-            <div class="carousel-inner">
+            <div class="col-md-12"><h1 id="methods">Methods</h1></div>
+            <div id="recipeCarousel" class="carousel slide" data-bs-ride="carousel">
+            <div class="carousel-inner" role="listbox">
                 <div class="carousel-item active">
-                <img src="..." class="d-block w-100" alt="...">
+                    <div class="col-md-4">
+                        <div class="card">
+                            <div class="card-img">
+                                <img src="//via.placeholder.com/500x400/31f?text=1" class="img-fluid">
+                            </div>
+                            <div class="card-img-overlay">Slide 1</div>
+                        </div>
+                    </div>
                 </div>
                 <div class="carousel-item">
-                <img src="..." class="d-block w-100" alt="...">
+                    <div class="col-md-4">
+                        <div class="card">
+                            <div class="card-img">
+                                <img src="//via.placeholder.com/500x400/e66?text=2" class="img-fluid">
+                            </div>
+                            <div class="card-img-overlay">Slide 2</div>
+                        </div>
+                    </div>
                 </div>
                 <div class="carousel-item">
-                <img src="..." class="d-block w-100" alt="...">
+                    <div class="col-md-4">
+                        <div class="card">
+                            <div class="card-img">
+                                <img src="//via.placeholder.com/500x400/7d2?text=3" class="img-fluid">
+                            </div>
+                            <div class="card-img-overlay">Slide 3</div>
+                        </div>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <div class="col-md-4">
+                        <div class="card">
+                            <div class="card-img">
+                                <img src="//via.placeholder.com/500x400?text=4" class="img-fluid">
+                            </div>
+                            <div class="card-img-overlay">Slide 4</div>
+                        </div>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <div class="col-md-4">
+                        <div class="card">
+                            <div class="card-img">
+                                <img src="//via.placeholder.com/500x400/aba?text=5" class="img-fluid">
+                            </div>
+                            <div class="card-img-overlay">Slide 5</div>
+                        </div>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <div class="col-md-4">
+                        <div class="card">
+                            <div class="card-img">
+                                <img src="//via.placeholder.com/500x400/fc0?text=6" class="img-fluid">
+                            </div>
+                            <div class="card-img-overlay">Slide 6</div>
+                        </div>
+                    </div>
                 </div>
             </div>
-            <button class="carousel-control-prev" type="button" data-bs-target="#methodsIndicators" data-bs-slide="prev">
+            <a class="carousel-control-prev bg-transparent w-aut" href="#recipeCarousel" role="button" data-bs-slide="prev">
                 <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Previous</span>
-            </button>
-            <button class="carousel-control-next" type="button" data-bs-target="#methodsIndicators" data-bs-slide="next">
+            </a>
+            <a class="carousel-control-next bg-transparent w-aut" href="#recipeCarousel" role="button" data-bs-slide="next">
                 <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Next</span>
-            </button>
-            </div>
+            </a>
+        </div>
+            
         </div>
 
 	</div><!-- .entry-content -->
